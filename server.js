2f6f47b8ab53dffd26226ee2c1f9df7a2787daf1
TOAN NGUYEN <ttngbt06@gmail.com>
2024-04-04 21:31:15 -0500

    Set up View function, welcome header

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ const db = mysql.createConnection(
     console.log(`Connected to the employees_db database.`)
 );
 
+db.connect(function (err) {
+    if (err) throw err;
+    console.log(|--------------------------------------|);
+    console.log(|           EMPLOYEE TRACKER           |);
+    console.log(|--------------------------------------|);
+    startingQuestion();
+});
+
 function startingQuestion() {
     inquirer.prompt([
         {
@@ -57,5 +65,41 @@ function startingQuestion() {
                 db.end();
                 break;
         }
+    });
+};
+
+function viewDepartments() {
+    const sql = `SELECT department.id, department.name AS Department FROM department;`
+    db.query(sql, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(res);
+        startingQuestion();
+    });
+};
+
+function viewRoles() {
+    const sql = `SELECT role.id, role.title AS role, role.salary, department.name AS department FROM role INNER JOIN department ON (department.id = role.department_id);`;
+    db.query(sql, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(res);
+        startingQuestion();
+    });
+};
+
+function viewEmployees() {
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN employee manager on manager.id = employee.manager_id INNER JOIN role ON (role.id = employee.role_id) INNER JOIN department ON (department.id = role.department_id) ORDER BY employee.id;`
+    db.query(sql, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(res);
+        startingQuestion();
     });
 };
